9a7017fe3f8d0b87af1fcdd2c31e99455cc85062
Akihiro Ononaka <antonio.kuhl.neto@escola.pr.gov.br>
2025-04-10 10:32:23 -0300

    Add files via upload

---
+++ src/aula02.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+class Aula02 extends Component{
+    render() {
+        return(
+            <View style={{flex: 1, backgroundColor:'black'}}>
+                <Text style={{color: 'white'}}>Aula 02 - Estilos</Text>
+            </View>
+        );
+    }
+}
+
+export default Aula02;
