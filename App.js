80fd29d701644ec5d1591162df83c4a4f7e1a4c8
Akihiro Ononaka <antonio.kuhl.neto@escola.pr.gov.br>
2025-06-25 10:35:25 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -4,17 +4,17 @@
 
 //import Aula03 from "./src/aula03";
 
-import Aula04 from "./src/aula04";
+//import Aula04 from "./src/aula04";
 
-//import Aula05 from "./src/aula05";
+import Aula05 from "./src/aula05";
 
 export default function App() {
   return (
     //<Aula01/>
     //<Aula02/>
     //<Aula03/>
-    <Aula04/>
-    //<Aula05/>
+    //<Aula04/>
+    <Aula05/>
   );
 }
 
