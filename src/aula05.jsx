3b43671bad7e0e8a69daa7d9a7bab7324eb3df4d
Aka Akasaka <antonio.kuhl.neto@escola.pr.gov.br>
2025-09-18 10:31:53 -0300

    Add files via upload

--- src/aula05.jsx
+++ src/aula05.jsx
@@ -1,39 +1,68 @@
-import React, {Component} from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
-class Aula05 extends Component{
-    render() {
-        return(
-            <View style= { styles.container }>
-                <View style= { styles.caixa1 } >
-                    <Text style={{ color:'white'}}>Say Gex</Text>
-                </View>
-                <View style= { styles.caixa2 } >2</View>
-                <View style= { styles.caixa3 } >3</View>
-            </View>
-        );
-    }
+class Aula05 extends Component {
+  render() {
+    return (
+      <View style={ styles.container }>
+        <View style={ styles.caixa1 }>MahouAkoToProSekaGaSukiDesu</View>
+        <View style={ styles.caixa2 }>
+          <Image 
+            style={ styles.img1 }
+            source={{ uri: 'https://media.tenor.com/nWpWC7Dxk8UAAAAm/pjsk-kanade.webp'}}
+          />
+        </View>
+        <View style={ styles.caixa3 }>
+          <Image 
+            source={ require('../img/hiiragi_utena.avif')}
+            style={ styles.img2}
+          />
+        </View>
+      </View>
+    );
+  }
 }
 
 export default Aula05;
 
-const styles = StyleSheet.create ({
+const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: 'white',
     },
 
     caixa1: {
-        backgroundColor: 'red',
-        height: 50,
+      height: 100,
+      backgroundColor: 'red',
+      alignItems: 'center',
+      justifyContent: 'center',
+      color: 'white',
+      fontSize: 20
     },
-
     caixa2: {
-        backgroundColor: 'green',
-        flex: 1,
+      flex: 1,
+      backgroundColor: 'green',
+      color: 'white',
+      fontSize: 20,
+      justifyContent: 'center',
+      alignItems: 'center'
     },
     caixa3: {
-        backgroundColor: 'blue',
-        height: 50,
+      backgroundColor: 'blue',
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      color: 'white',
+      fontSize: 20
+    },
+
+    img1: {
+      width: 200,
+      height: 200,
+    },
+
+    img2: {
+        width: 100,
+        height: 200,
     },
 })
