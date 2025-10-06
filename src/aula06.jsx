07d65247d6702854b32ee56e9f1e133de073c7b1
Aka Akasaka <antonio.kuhl.neto@escola.pr.gov.br>
2025-10-06 09:22:47 -0300

    Add files via upload

---
+++ src/aula06.jsx
@@ -0,0 +1,59 @@
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+
+class Aula06 extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            nome: '',
+        };
+        this.pegaNome = this.pegaNome.bind(this);
+    }
+
+    pegaNome(texto) {
+        if(texto.lenght > 0) {
+            this.state.State({nome: "Bem Vindo: " + texto})
+        }
+        else {
+            this.setState({nome:''})
+        }
+    }
+
+    render () {
+        return(
+            <View style={StyleSheet.conteiner}>
+                <TextInput 
+                style={styles.imput}
+                placeholder='Digite seu nome:'
+                underlineColorAndroid="transparent"
+                onChangeText={this.pegaNome}
+                />
+
+                <Text style={styles.texto}>{this.state.nome}</Text>
+            </View>
+        );
+    }
+}
+
+export default Aula06;
+
+const styles = StyleSheet.create ({
+    conteiner: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    input: {
+        height: 45,
+        borderWidth: 1,
+        borderColor: '#222',
+        margin: 10,
+        fontSize: 20,
+        padding: 10,
+    },
+
+    texto: {
+        textAlign: 'center',
+        fontSize: 25,
+    }
+});
